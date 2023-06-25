b6da217f8f4ffa215b69641d9548652a0b742626
tahsib <tahsib7@gmail.com>
2023-06-25 23:37:13 +0600

    vanilla js implementation added

---
+++ vanilla-javascript/index.js
@@ -0,0 +1,22 @@
+document.getElementById("phone").oninput = function() {format_number()};
+
+function format_number(){
+    const phone = document.getElementById("phone");
+
+    phone.value = phone.value.replace('(', '');
+    phone.value =  phone.value.replace(')', '');
+    phone.value =  phone.value.replace('-', '');
+
+    let formattedInput = phone.value
+    formattedInput = formattedInput.split("")
+
+    if (formattedInput.length > 3) {
+        formattedInput = ['(', ...formattedInput.slice(0,3), ')', ...formattedInput.slice(3)]
+    }
+    
+    if (formattedInput.length > 8) {
+        formattedInput = [...formattedInput.slice(0,8), '-', ...formattedInput.slice(8)]
+    }
+    phone.value = formattedInput.join("")
+}
+
