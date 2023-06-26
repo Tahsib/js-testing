cd72c6ae8d50303f9c1b6406b4b40b5f5961c9ae
Tahsib Ahad Chowdhury <tahsib.ahad@reddotdigitalit.com>
2023-06-26 15:09:14 +0600

    reactjs implementation added

---
+++ reactjs/src/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState } from "react";
+import "./style.css";
+
+function formatNumber(input, prevInput) {
+  let value = input.value;
+  let pos = input.selectionStart;
+  console.log(pos);
+  console.log(prevInput);
+
+  value = value.replace(/[()-]/g, "");
+
+  let formattedInput = value;
+  formattedInput = formattedInput.split("");
+
+  if (formattedInput.length > 3) {
+    formattedInput = [
+      "(",
+      ...formattedInput.slice(0, 3),
+      ")",
+      ...formattedInput.slice(3)
+    ];
+  }
+
+  if (formattedInput.length > 8) {
+    formattedInput = [
+      ...formattedInput.slice(0, 8),
+      "-",
+      ...formattedInput.slice(8)
+    ];
+  }
+
+  if (!prevInput.includes("(") && formattedInput.includes("(")) {
+    pos += 1;
+  }
+  if (!prevInput.includes(")") && formattedInput.includes(")")) {
+    pos += 1;
+  }
+  if (!prevInput.includes("-") && formattedInput.includes("-")) {
+    pos += 1;
+  }
+  prevInput = formattedInput;
+  value = formattedInput.join("");
+  return [value, pos, prevInput];
+}
+
+function App() {
+  const initialValues = {
+    prevInput: [],
+    currentVal: ""
+  };
+  const [values, setValues] = useState(initialValues);
+
+  return (
+    <div className="App">
+      <div className="container text-center">
+        <input
+          type="tel"
+          id="phone"
+          maxLength={16}
+          placeholder="mobile number"
+          autoComplete="off"
+          defaultValue={values.currentVal}
+          onInput={(e) => {
+            let [newNumber, newPos, prev] = formatNumber(
+              e.target,
+              values.prevInput
+            );
+            setValues({
+              prevInput: prev
+            });
+            e.target.value = newNumber;
+            e.target.setSelectionRange(newPos, newPos);
+          }}
+        />
+        <div>
+          <label htmlFor="phone">(123) 456-7890</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
