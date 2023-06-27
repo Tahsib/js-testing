41c94d5b5fbc1936ad5f16ae84c245fffe5488a7
Munif Tanjim <hello@muniftanjim.dev>
2023-06-27 21:22:27 +0600

    merge test files

---
+++ reactjs/src/App.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("should format phone number correctly", () => {
+    const { getByLabelText } = render(<App />);
+    const phoneInput = getByLabelText("(123) 456-7890");
+
+    fireEvent.change(phoneInput, { target: { value: "1234567890" } });
+    expect(phoneInput.value).toBe("(123) 456-7890");
+
+    fireEvent.change(phoneInput, { target: { value: "1234567890123456" } });
+    expect(phoneInput.value).toBe("(123) 456-7890");
+
+    fireEvent.change(phoneInput, { target: { value: "123a456b7890" } });
+    expect(phoneInput.value).toBe("(123) 456-7890");
+
+    fireEvent.change(phoneInput, { target: { value: "1234567890abcd" } });
+    expect(phoneInput.value).toBe("(123) 456-7890");
+  });
+
+  it("should put cursor at right place", () => {
+    const { getByLabelText } = render(<App />);
+    const phoneInput = getByLabelText("(123) 456-7890");
+
+    fireEvent.change(phoneInput, { target: { value: "(123) 456-7890" } });
+    expect(phoneInput.value).toBe("(123) 456-7890");
+    expect(phoneInput.selectionStart).toBe(5);
+  });
+});
