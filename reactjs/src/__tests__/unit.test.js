508febd326d083d8f6d61a2c5bbfcb131a968649
Munif Tanjim <hello@muniftanjim.dev>
2023-06-27 21:17:47 +0600

    react test setup

--- reactjs/src/__tests__/unit.test.js
+++ reactjs/src/__tests__/unit.test.js
@@ -1,7 +1,6 @@
-import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
-import App from '../index.js'
+import App from '../App'
 
 describe('App', () => {
   it('should format phone number input correctly', () => {
